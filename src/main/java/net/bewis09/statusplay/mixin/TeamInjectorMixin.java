e3edc5b92d06dcbdc8038cc5d80690c7298d51d4
Bewis09 <bewis09minecraft@gmail.com>
2025-03-25 07:34:42 +0100

    Update Minecraft and Fabric API versions; refactor status management to use new registry access

--- src/main/java/net/bewis09/statusplay/mixin/TeamInjectorMixin.java
+++ src/main/java/net/bewis09/statusplay/mixin/TeamInjectorMixin.java
@@ -1,6 +1,7 @@
 package net.bewis09.statusplay.mixin;
 
 import net.bewis09.statusplay.StatusPlay;
+import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.scoreboard.ServerScoreboard;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,8 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class TeamInjectorMixin {
 	@Shadow public abstract ServerScoreboard getScoreboard();
 
+	@Shadow public abstract DynamicRegistryManager.Immutable getRegistryManager();
+
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
-		StatusPlay.INSTANCE.reloadStatusTeams(this.getScoreboard());
+		StatusPlay.INSTANCE.reloadStatusTeams(this.getScoreboard(), this.getRegistryManager());
 	}
 }
--- src/main/kotlin/net/bewis09/statusplay/StatusPlay.kt
+++ src/main/kotlin/net/bewis09/statusplay/StatusPlay.kt
@@ -14,8 +14,8 @@ import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.command.argument.ColorArgumentType
 import net.minecraft.command.argument.EntityArgumentType
 import net.minecraft.command.argument.TextArgumentType
+import net.minecraft.registry.RegistryWrapper
 import net.minecraft.scoreboard.ServerScoreboard
-import net.minecraft.scoreboard.Team
 import net.minecraft.server.MinecraftServer
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
@@ -32,29 +32,12 @@ object StatusPlay : ModInitializer {
 	private val gson = Gson()
 	val statusList: ArrayList<Status> = ArrayList()
 
+	private var file_loaded = false
+
 	private val logger: Logger = LoggerFactory.getLogger("StatusPlay")
 
 	override fun onInitialize() {
-		val file = File(FabricLoader.getInstance().configDir.toString(), "statusplay.json")
-
-		if (!file.exists()) {
-			file.createNewFile()
-			file.writeText("[]")
-		}
-
-		try {
-			gson.fromJson(file.readText(), JsonArray().javaClass).forEach {
-				val obj = it.asJsonObject
-				val text = Text.Serializer.fromJson(obj["text"].asString)
-
-				if (text != null)
-					statusList.add(Status(obj["name"].asString, text))
-			}
-		} catch (_: Exception) {
-			logger.error("Failed to load statusplay.json")
-		}
-
-		CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
+		CommandRegistrationCallback.EVENT.register { dispatcher, registryAccess, _ ->
 			dispatcher.register(
 				CommandManager.literal("status").then(
 					CommandManager.argument("status", StringArgumentType.word()).suggests(
@@ -74,7 +57,7 @@ object StatusPlay : ModInitializer {
 						}
 
 						val server = source.server
-						server.scoreboard.addPlayerToTeam(source.name, server.scoreboard.getTeam(team.name))
+						server.scoreboard.addScoreHolderToTeam(source.name, server.scoreboard.getTeam(team.name))
 						source.sendFeedback({ Text.literal("Your status has been set to ").append(team.text) }, false)
 						1
 					}
@@ -86,14 +69,14 @@ object StatusPlay : ModInitializer {
 					}
 
 					val server = source.server
-					server.scoreboard.removePlayerFromTeam(source.name, server.scoreboard.getPlayerTeam(source.name))
+					server.scoreboard.removeScoreHolderFromTeam(source.name, server.scoreboard.getScoreHolderTeam(source.name))
 					source.sendFeedback({ Text.literal("Your status has been removed") }, false)
 					1
 				}
 			)
 
 			dispatcher.register(
-				CommandManager.literal("ms").requires { it.player?.hasPermissionLevel(2) == true }.then(
+				CommandManager.literal("ms").requires { it.player?.hasPermissionLevel(2) != false }.then(
 					CommandManager.literal("add").then(
 						CommandManager.argument(
 							"id", StringArgumentType.word()
@@ -102,14 +85,14 @@ object StatusPlay : ModInitializer {
 							val id = it.getArgument("id", String::class.java)
 							val name = it.getArgument("name", String::class.java)
 							val text = Text.literal("[$name]").formatted(it.getArgument("color", Formatting::class.java))
-							addStatus(id, text, it.source.server)
+							addStatus(id, text, it.source.server, registryAccess)
 							source.sendFeedback({ Text.literal("Added status ").append(text) }, false)
 							1
-						}))).then(CommandManager.literal("custom").then(CommandManager.argument("text", TextArgumentType.text()).executes {
+						}))).then(CommandManager.literal("custom").then(CommandManager.argument("text", TextArgumentType.text(registryAccess)).executes {
 							val source = it.source
 							val id = it.getArgument("id", String::class.java)
 							val text = it.getArgument("text", Text::class.java)
-							addStatus(id, text.copy(), it.source.server)
+							addStatus(id, text.copy(), it.source.server, registryAccess)
 							source.sendFeedback({ Text.literal("Added status ").append(text) }, false)
 							1
 						}))
@@ -125,7 +108,7 @@ object StatusPlay : ModInitializer {
 								source.sendFeedback({ Text.literal("Invalid status").formatted(Formatting.RED) }, false)
 								return@executes 0
 							}
-							removeStatus(id, it.source.server)
+							removeStatus(id, it.source.server, registryAccess)
 							source.sendFeedback({ Text.of("Removed status $id") }, false)
 							1
 						})
@@ -142,14 +125,14 @@ object StatusPlay : ModInitializer {
 								source.sendFeedback({ Text.literal("Invalid status").formatted(Formatting.RED) }, false)
 								return@executes 0
 							}
-							it.source.server.scoreboard.addPlayerToTeam(player.gameProfile.name, it.source.server.scoreboard.getTeam(team.name))
+							it.source.server.scoreboard.addScoreHolderToTeam(player.gameProfile.name, it.source.server.scoreboard.getTeam(team.name))
 							source.sendFeedback({ Text.literal("Set status of ").append(player.name).append(" to ").append(team.text) }, false)
 							1
 						}).executes {
 							val source = it.source
 							val player = EntityArgumentType.getPlayer(it, "player")
 							val server = source.server
-							server.scoreboard.removePlayerFromTeam(player.gameProfile.name, server.scoreboard.getPlayerTeam(player.gameProfile.name))
+							server.scoreboard.removeScoreHolderFromTeam(player.gameProfile.name, server.scoreboard.getScoreHolderTeam(player.gameProfile.name))
 							source.sendFeedback({ Text.literal("Removed status of ").append(player.name) }, false)
 							1
 						}
@@ -159,26 +142,26 @@ object StatusPlay : ModInitializer {
 		}
 	}
 
-	private fun addStatus(text: String, name: MutableText, server: MinecraftServer) {
+	private fun addStatus(text: String, name: MutableText, server: MinecraftServer, registryWrapper: RegistryWrapper.WrapperLookup) {
 		statusList.removeIf { it.name == text }
 		statusList.add(Status(text.lowercase().replace(" ","_"), name))
-		writeStatus()
-		reloadStatusTeams(server.scoreboard)
+		writeStatus(registryWrapper)
+		reloadStatusTeams(server.scoreboard, registryWrapper)
 	}
 
-	private fun removeStatus(name: String, server: MinecraftServer) {
+	private fun removeStatus(name: String, server: MinecraftServer, registryWrapper: RegistryWrapper.WrapperLookup) {
 		statusList.removeIf { it.name == name }
-		writeStatus()
+		writeStatus(registryWrapper)
 		server.scoreboard.removeTeam(server.scoreboard.getTeam(name))
-		reloadStatusTeams(server.scoreboard)
+		reloadStatusTeams(server.scoreboard, registryWrapper)
 	}
 
-	private fun writeStatus() {
+	private fun writeStatus(registryWrapper: RegistryWrapper.WrapperLookup) {
 		val file = File(FabricLoader.getInstance().configDir.toString(), "statusplay.json")
 		file.writeText(gson.toJson(statusList.map { status: Status ->
 			val obj = JsonObject()
 			obj.addProperty("name", status.name)
-			obj.addProperty("text", Text.Serializer.toJson(status.text))
+			obj.addProperty("text", Text.Serialization.toJsonString(status.text, registryWrapper))
 			obj
 		}))
 	}
@@ -194,7 +177,30 @@ object StatusPlay : ModInitializer {
 		}
 	}
 
-	fun reloadStatusTeams(scoreboard: ServerScoreboard) {
+	fun reloadStatusTeams(scoreboard: ServerScoreboard, registryWrapper: RegistryWrapper.WrapperLookup) {
+		if(!file_loaded) {
+			file_loaded = true
+
+			val file = File(FabricLoader.getInstance().configDir.toString(), "statusplay.json")
+
+			if (!file.exists()) {
+				file.createNewFile()
+				file.writeText("[]")
+			}
+
+			try {
+				gson.fromJson(file.readText(), JsonArray().javaClass).forEach {
+					val obj = it.asJsonObject
+					val text = Text.Serialization.fromJson(obj["text"].asString, registryWrapper)
+
+					if (text != null)
+						statusList.add(Status(obj["name"].asString, text))
+				}
+			} catch (_: Exception) {
+				logger.error("Failed to load statusplay.json")
+			}
+		}
+
 		statusList.forEach(Consumer { status: Status ->
 			var team = scoreboard.getTeam(status.name)
 			if (team == null) {
